7fc7da0e8795363a858644206e895f49511caea8
Gawyn <me@cristianplanas.com>
2014-07-04 21:17:26 +0200

    Basic article model

--- api/models/Article.js
+++ api/models/Article.js
@@ -1,19 +1,12 @@
-/**
- * Article
- *
- * @module      :: Model
- * @description :: A short summary of how this model works and what it represents.
- * @docs		:: http://sailsjs.org/#!documentation/models
- */
-
 module.exports = {
-
   attributes: {
-  	
-  	/* e.g.
-  	nickname: 'string'
-  	*/
-    
+    title: {
+      type: 'string',
+      required: true
+    }
+    body: {
+      type: 'text',
+      required: true
+    }
   }
-
 };
